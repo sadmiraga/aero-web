052479dfcf97341d1472fb09370294e129dffd66
sadmiraga <sadmirvela@gmail.com>
2021-10-31 21:17:06 +0100

    navbar done

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -53,12 +53,12 @@
 
     <nav class="main-navbar navbar navbar-expand-lg navbar-light bg-light">
 
-        <a class="navbar-brand" href="/"><img width="100" src="images/logo-white.png"></a>
+        <a class="navbar-brand" href="/"><img class="navbar-brand-image" src="images/logo-white.png"></a>
 
         <button class="navbar-toggler dropdown-button" type="button" data-toggle="collapse"
             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
             aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span><i class="fas fa-bars"></i></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -80,10 +80,10 @@
             </ul>
 
             <div class="language-container my-2 my-lg-0">
-                <img width="50" src="images/languages/slovenian.png" class="language-icon">
-                <img width="50" src="images/languages/english.jpg" class="language-icon">
-                <img width="50" src="images/languages/croatian.png" class="language-icon">
-                <img width="50" src="images/languages/german.png" class="language-icon">
+                <div class="language-box" id="slovenian"></div>
+                <div class="language-box" id="english"></div>
+                <div class="language-box" id="german"></div>
+                <div class="language-box" id="croatian"></div>
             </div>
         </div>
     </nav>
